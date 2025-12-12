18c27b6f3367c82b1e17b908edf0615c28aad4a8
Johandermeistero38 <johanvanbelen@gmail.com>
2025-12-12 10:32:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+st.set_page_config(page_title="Facturen Checker V2", layout="wide")
+st.title("🔍 Facturen Checker – TOPPOINT (V2)")
+
+st.success("Basis app draait correct 🎉")
